cae586204ac81a9fc0f666d75e19fd2966db88a9
Michael.Nodzenski <michael.nodzenski@gmail.com>
2019-11-25 11:35:37 -0500

    Weight by 2x total different snps and 1x number both 1.

--- R/fitness.score.R
+++ R/fitness.score.R
@@ -94,8 +94,7 @@ fitness.score <- function(case.genetic.data, complement.genetic.data, case.comp.
 
     #compute weights
     both.one <- rowSums(both.one.mat[ , target.snps])
-    #family.weights <- both.one + total.different.snps
-    family.weights <- 1 + both.one
+    family.weights <- both.one + 2*total.different.snps
 
     #weighted difference vectors between cases and complements
     dif.vecs <- family.weights*cases.minus.complements[ , target.snps]
@@ -115,7 +114,6 @@ fitness.score <- function(case.genetic.data, complement.genetic.data, case.comp.
     #fitness score as the ratio of the observed to expected squared vector length, inverse weigthed by
     #1 plus the variance of the absolute value of elements of the vector
     fitness.score <- (1/(1 + dif.vec.sd))*(sq.length.sum.dif.vecs/(expected.sq.length.sum.dif.vecs))
-
 
   }
 
