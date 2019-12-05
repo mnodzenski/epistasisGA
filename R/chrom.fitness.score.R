ada14c402d07e3849df8bd7b9913a8207aaef200
mnodzenski <michael.nodzenski.nih.gov>
2019-12-05 09:59:48 -0500

    Adding in parameters to change the weighting of the number of different snps and the number both equal to 1.

--- R/chrom.fitness.score.R
+++ R/chrom.fitness.score.R
@@ -6,6 +6,8 @@
 #' @param target.snps A numeric vector of the columns corresponding to the snps for which the fitness score will be computed.
 #' @param cases.minus.complements A matrix equal to case genetic data - complement genetic data.
 #' @param both.one.mat A matrix whose elements indicate whether both the case and complement have one copy of the alternate allele, equal to (case.genetic.data == 1 & complement.genetic.data == 1).
+#' @param n.different.snps.weight The number by which the number different snps between case and control is multiplied in computing the family weights. Defaults to 2.
+#' @param n.both.one.weight The number by which the number of snps equal to 1 in both case and control is multiplied in computing the family weights. Defaults to 1.
 #' @return A scalar, the fitness score for the given set of snps.
 #'
 #' @examples
@@ -21,7 +23,8 @@
 #'
 #' @export
 
-chrom.fitness.score <- function(case.comp.differences, target.snps, cases.minus.complements, both.one.mat){
+chrom.fitness.score <- function(case.comp.differences, target.snps, cases.minus.complements, both.one.mat,
+                                n.different.snps.weight = 2, n.both.one.weight = 1){
 
   ### pick out the differences for the target snps ###
   case.comp.diff <- case.comp.differences[ , target.snps]
@@ -33,7 +36,7 @@ chrom.fitness.score <- function(case.comp.differences, target.snps, cases.minus.
 
   ### compute weights ###
   both.one <- rowSums(both.one.mat[informative.families , target.snps])
-  family.weights <- both.one + 2*total.different.snps[informative.families]
+  family.weights <- n.both.one.weight*both.one + n.different.snps.weight*total.different.snps[informative.families]
   family.weights  <- family.weights/sum(family.weights)
 
   ### compute weighted difference vectors for cases vs complements ###
