7526c5c515652b1301f0f95efd43c1887f3b32c1
mnodzenski <michael.nodzenski.nih.gov>
2019-12-15 17:00:30 -0500

    Correct the name of the complement genetic data.

--- R/run.ga.R
+++ R/run.ga.R
@@ -3,7 +3,7 @@
 #' This function runs a genetic algorithm on a collection of candidate snp sets, to identify epistatic variants.
 #'
 #' @param case.genetic.data A genetic dataset from cases (for a dichotomous trait). Columns are snps, and rows are individuals.
-#' @param comp.genetic.data A genetic dataset from the complements of the cases, where \code{comp.genetic.data} = mother snp counts + father snp counts - case snp counts. Columns are snps, rows are families. If not specified, \code{father.genetic.data} and \code{mother.genetic.data} must be specified.
+#' @param complement.genetic.data A genetic dataset from the complements of the cases, where \code{complement.genetic.data} = mother snp counts + father snp counts - case snp counts. Columns are snps, rows are families. If not specified, \code{father.genetic.data} and \code{mother.genetic.data} must be specified.
 #' @param father.genetic.data The genetic data for the father of the case. Columns are snps, rows are individuals. Does not need to be specified if \code{comp.genetic.data} is specified.
 #' @param mother.genetic.data The genetic data for the mother of the case. Columns are snps, rows are individuals. Does not need to be specified if \code{comp.genetic.data} is specified.
 #' @param n.chromosomes A scalar indicating the number of candidate collections of snps to use in the GA.
@@ -32,13 +32,13 @@
 #' @importFrom data.table data.table rbindlist setorder
 #' @export
 
-run.ga <- function(case.genetic.data, comp.genetic.data = NULL, father.genetic.data = NULL, mother.genetic.data = NULL,
+run.ga <- function(case.genetic.data, complement.genetic.data = NULL, father.genetic.data = NULL, mother.genetic.data = NULL,
                    n.chromosomes, chromosome.size, seed.val,n.different.snps.weight = 2, n.both.one.weight = 1,
                    weight.function = identity, min.allele.freq = 0.01, generations = 2000, gen.same.fitness = 500,
                    min.n.risk.set = 10, tol = 10^-6, n.top.chroms = 100){
 
   #make sure the appropriate genetic data is included
-  if (is.null(comp.genetic.data) & is.null(father.genetic.data) & is.null(mother.genetic.data)){
+  if (is.null(complement.genetic.data) & is.null(father.genetic.data) & is.null(mother.genetic.data)){
 
     stop("Must include comp.genetic.data or both father.genetic.data and mother.genetic.data")
 
@@ -64,7 +64,7 @@ run.ga <- function(case.genetic.data, comp.genetic.data = NULL, father.genetic.d
     ### Compute the complement data ###
     complement.genetic.data <- father.genetic.data + mother.genetic.data - case.genetic.data
 
-  } else if (!is.null(comp.genetic.data)){
+  } else if (!is.null(complement.genetic.data)){
 
     alt.allele.freqs <- colSums(case.genetic.data + complement.genetic.data)/(4*nrow(case.genetic.data))
     below.maf.threshold <- alt.allele.freqs > (1 - min.allele.freq) | alt.allele.freqs < min.allele.freq
