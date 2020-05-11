5b6fc81be5f4c856158747e7a529be0c2056ab86
mnodzenski <michael.nodzenski.nih.gov>
2020-05-11 15:12:48 -0400

    Update plotting function.

--- R/network.plot.R
+++ R/network.plot.R
@@ -2,19 +2,21 @@
 #'
 #' This function rplot a network of potential epistatic snps.
 #'
-#' @param results.df A data frame of results of GA runs. The first n columns are snp ids, the next n columns are there associated mean vector components.
+#' @param results.df The \code{unique.results} data frame of results of GA runs from \code{combine.islands}, with fitness scores restricted to the top results using function \code{network.threshold}.
 #' @param chrom.size An integer indicating the number of snps in the chromosome.
 #' @param seed an integer specifying the graph seed
-#' @param n.top.chroms An integer indicating the number of top chromosomes to plot
 #' @return A list whose first element is the fitness score and second element is the sum of weighted difference vectors for the target snps.
 #'
 #' @importFrom  dplyr group_by summarize %>%
 #' @import igraph
 #' @export
 
-network.plot <- function(results.df, chrom.size, seed = 10, n.top.chroms = 50){
+network.plot <- function(results.df, chrom.size, seed = 10){
 
   set.seed(seed)
+  n.top.chroms <- nrow(results.df)
+  result.df$h.score <- results.df$fitness.score*results.df$n.islands.found
+
   all.edge.weights <- do.call(rbind, lapply(1:n.top.chroms, function(res.row){
 
     chrom.res <- results.df[res.row, ]
@@ -35,17 +37,18 @@ network.plot <- function(results.df, chrom.size, seed = 10, n.top.chroms = 50){
 
       }
 
-      pair1.pos <- which(chrom.res[ , 1:chrom.size] == pair1)
-      pair2.pos <- which(chrom.res[ , 1:chrom.size] == pair2)
-      standardized.elements <- as.vector(t(chrom.res[ , (1+chrom.size):(2*chrom.size)]))
-      vec.length.sq <- sum(standardized.elements^2)
-      pair1.val <- standardized.elements[pair1.pos]
-      pair2.val <- standardized.elements[pair2.pos]
-      pair.contrib <- sum(standardized.elements[c(pair1.pos, pair2.pos)]^2)
-      pair.weight <- (pair.contrib/vec.length.sq)*chrom.res$fitness.score
-      pair$edge.weight <-  1/pair.weight
-      pair$var1.val <- pair1.val
-      pair$var2.val <- pair2.val
+      #pair1.pos <- which(chrom.res[ , 1:chrom.size] == pair1)
+      #pair2.pos <- which(chrom.res[ , 1:chrom.size] == pair2)
+      #standardized.elements <- as.vector(t(chrom.res[ , (1+chrom.size):(2*chrom.size)]))
+      #vec.length.sq <- sum(standardized.elements^2)
+      #pair1.val <- standardized.elements[pair1.pos]
+      #pair2.val <- standardized.elements[pair2.pos]
+      #pair.contrib <- sum(standardized.elements[c(pair1.pos, pair2.pos)]^2)
+      #pair.weight <- (pair.contrib/vec.length.sq)*chrom.res$fitness.score
+      pair.weight <- chrom.res$h.score
+      pair$edge.weight <-  pair.weight
+      #pair$var1.val <- pair1.val
+      #pair$var2.val <- pair2.val
       return(pair)
 
     }))
@@ -54,21 +57,22 @@ network.plot <- function(results.df, chrom.size, seed = 10, n.top.chroms = 50){
 
   }))
 
-
-
   final.edge.weights <- all.edge.weights %>%
-    group_by(Var1, Var2) %>%
-    summarize(max.edge.weight = max(edge.weight)) %>%
-    as.data.frame()
-  node.scores <- data.frame(name = unlist(results.df[1:n.top.chroms, 1:chrom.size]), estimate = unlist(results.df[1:n.top.chroms, (1 + chrom.size):(2*chrom.size)]))
-  max.node.scores <- node.scores %>%
+                          group_by(Var1, Var2) %>%
+                          summarize(edge.weight = 1/sum(edge.weight)) %>%
+                          as.data.frame()
+
+  node.scores <- data.frame(name = unlist(results.df[1:n.top.chroms, 1:chrom.size]),
+                            h.score = unlist(results.df[1:n.top.chroms, "h.score"]))
+
+  sum.node.scores <- node.scores %>%
     group_by(name) %>%
-    summarize(size = exp(5*abs(max(estimate)))) %>%
+    summarize(size = sum(h.score)) %>%
     as.data.frame()
 
   colnames(final.edge.weights)[1:2] <- c("from", "to")
-  network <- graph.data.frame(final.edge.weights[ , 1:2], directed = F, vertices = max.node.scores)
-  E(network)$weight <- final.edge.weights$max.edge.weight
+  network <- graph.data.frame(final.edge.weights[ , 1:2], directed = F, vertices = sum.node.scores)
+  E(network)$weight <- final.edge.weights$edge.weight
   color_fun <- colorRampPalette(c('red', 'grey', 'white'))
   required.colors <- as.integer(as.factor(E(network)$weight))
   colors <- color_fun(length(required.colors))
