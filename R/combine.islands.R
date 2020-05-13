f67a66be73255684d10bcc010f1d3c230032a6b3
mnodzenski <michael.nodzenski@gmail.com>
2020-05-13 18:03:38 -0400

    Add example for combine.islands.

--- R/combine.islands.R
+++ R/combine.islands.R
@@ -4,8 +4,30 @@
 #'
 #' @param results.dir The directory in which individual island results from \code{run.ga} are saved.
 #' @return A list of two elements, \code{all.results} and \code{unique.results}. \code{all.results} contains chromosome results across all islands, including chromsomes identified on more than one island. \code{unique.results} removes duplicate chromosomes found on more than one island. These two objects will also be written to \code{results.dir} as combined.island.results.rds and combined.island.unique.chromosome.results.rds.
+#' @examples
+#'
+#' data(case)
+#' data(dad)
+#' data(mom)
+#' library(Matrix)
+#' chrom.mat <- as.matrix(bdiag(list(matrix(rep(TRUE, 2500^2), nrow = 2500),
+#'                               matrix(rep(TRUE, 2500^2), nrow = 2500),
+#'                               matrix(rep(TRUE, 2500^2), nrow = 2500),
+#'                               matrix(rep(TRUE, 2500^2), nrow = 2500))))
+#'
+#' pp.list <- preprocess.genetic.data(case[, 1:10], father.genetic.data = dad[ , 1:10],
+#'                                mother.genetic.data = mom[ , 1:10],
+#'                                chrom.mat = chrom.mat[ , 1:10])
+#'
+#' run.ga(pp.list, n.chromosomes = 4, chromosome.size = 3, results.dir = "tmp",
+#'        cluster.type = "interactive", registryargs = list(file.dir = "tmp_reg", seed = 1500),
+#'        generations = 2, n.islands = 2, island.cluster.size = 1, n.top.chroms = 3)
+#'
+#' combined.res <- combine.islands("tmp")
+#'
 #' @importFrom data.table rbindlist setkey setorder
 #' @export
+
 combine.islands <- function(results.dir){
 
   #list all islands in the results data
