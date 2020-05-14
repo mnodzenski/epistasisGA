480cee8765512dfc468f0e94d8e83f1a13d977b4
Michael.Nodzenski <michael.nodzenski.nih.gov>
2020-05-14 18:40:34 -0400

    Document included datasets.

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' SNP counts for the mothers of case-parent triads.
+#'
+#' A dataset containing the counts of the alternate allele
+#' for 100 SNPs for the mothers in 1000 simulated case-parent
+#' triads. Columns represent snps, rows are individuals.
+#'
+#' @format A data frame with 1000 rows and 100 variables
+"mom"
+
+#' SNP counts for the fathers of case-parent triads.
+#'
+#' A dataset containing the counts of the alternate allele
+#' for 100 SNPs for the fathers in 1000 simulated case-parent
+#' triads. Columns represent snps, rows are individuals.
+#'
+#' @format A data frame with 1000 rows and 100 variables
+"dad"
+
+#' SNP counts for the affected children of case-parent triads.
+#'
+#' A dataset containing the counts of the alternate allele
+#' for 100 SNPs for the affected child in 1000 simulated case-parent
+#' triads. Columns represent snps, rows are individuals.
+#'
+#' @format A data frame with 1000 rows and 100 variables
+"case"
