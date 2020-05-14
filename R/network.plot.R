27502ce79df0fde60308609b4b0bc7331e47c25a
mnodzenski <michael.nodzenski.nih.gov>
2020-05-14 10:23:07 -0400

    Add example to network.plot.

--- R/network.plot.R
+++ R/network.plot.R
@@ -11,6 +11,27 @@
 #' @param plot A logical indicating whether the network should be plotted. If set to false, this function will return an igraph object which can be used for manual plotting.
 #' @return An igraph object, if \code{plot} is set to FALSE.
 #'
+#' data(case)
+#' data(dad)
+#' data(mom)
+#' library(Matrix)
+#' chrom.mat <- as.matrix(bdiag(list(matrix(rep(TRUE, 2500^2), nrow = 2500),
+#'                               matrix(rep(TRUE, 2500^2), nrow = 2500),
+#'                               matrix(rep(TRUE, 2500^2), nrow = 2500),
+#'                               matrix(rep(TRUE, 2500^2), nrow = 2500))))
+#'
+#' pp.list <- preprocess.genetic.data(case[, 1:10], father.genetic.data = dad[ , 1:10],
+#'                                mother.genetic.data = mom[ , 1:10],
+#'                                chrom.mat = chrom.mat[ , 1:10])
+#'
+#' run.ga(pp.list, n.chromosomes = 4, chromosome.size = 3, results.dir = "tmp",
+#'        cluster.type = "interactive", registryargs = list(file.dir = "tmp_reg", seed = 1500),
+#'        generations = 2, n.islands = 2, island.cluster.size = 1, n.top.chroms = 3)
+#'
+#' combined.res <- combine.islands("tmp")
+#'
+#' network.plot(combined.res$unique.results)
+#'
 #' @importFrom  dplyr group_by summarize %>%
 #' @import igraph
 #' @importFrom qgraph qgraph.layout.fruchtermanreingold
@@ -78,7 +99,7 @@ network.plot <- function(results.df, repulse.rad = 1000, node.size = 25, graph.a
 
     net.edges <- get.edgelist(network, names = F)
     coords <- qgraph.layout.fruchtermanreingold(net.edges, vcount = vcount(network),
-                                                repulse.rad = repulse.rad*vcount(g),
+                                                repulse.rad = repulse.rad*vcount(network),
                                                 area = graph.area*(vcount(network)^2))
     plot(network, layout = coords, vertex.label.cex = vertex.label.cex, asp = 0)
 
