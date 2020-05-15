d3f0145ccb6988cfbaf6826b4b3c7b746e4d6219
Michael.Nodzenski <michael.nodzenski@gmail.com>
2020-05-15 11:59:58 -0400

    Add in description file to quiet check about visible bindings of global variables.

---
+++ R/knnGA.R
@@ -0,0 +1,17 @@
+#' \code{knnGA} package
+#'
+#' A package implementing a genetic algorithm to detect multi-SNP effects in case-parent triad studies.
+#'
+#' @docType package
+#' @name knnGA
+NULL
+
+## quiets concerns of R CMD check re visible bindings of global vars
+## created via data table
+if(getRversion() >= "2.15.1"){
+
+  utils::globalVariables(c("chromosome", ".SD", "fitness.score",
+                           "raw.fitness.score", "min.elem", "Var1",
+                           "Var2", "name", "cluster", "job.id"))
+
+}
