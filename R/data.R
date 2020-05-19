3882492c52097b1cede4f680db0b6d5de45cb89c
Michael.Nodzenski <michael.nodzenski.nih.gov>
2020-05-19 17:55:22 -0400

    Update example datasets to include a true risk pathway.

--- R/data.R
+++ R/data.R
@@ -1,26 +1,29 @@
 #' SNP counts for the mothers of case-parent triads.
 #'
-#' A dataset containing the counts of the alternate allele
+#' A simulated dataset containing the counts of the alternate allele
 #' for 100 SNPs for the mothers in 1000 simulated case-parent
-#' triads. Columns represent snps, rows are individuals.
+#' triads. Columns represent snps, rows are individuals. SNPs
+#' in columns 51, 52, 76, and 77 represent a true risk pathway.
 #'
 #' @format A data frame with 1000 rows and 100 variables
 "mom"
 
 #' SNP counts for the fathers of case-parent triads.
 #'
-#' A dataset containing the counts of the alternate allele
+#' A simulated dataset containing the counts of the alternate allele
 #' for 100 SNPs for the fathers in 1000 simulated case-parent
-#' triads. Columns represent snps, rows are individuals.
+#' triads. Columns represent snps, rows are individuals. SNPs
+#' in columns 51, 52, 76, and 77 represent a true risk pathway.
 #'
 #' @format A data frame with 1000 rows and 100 variables
 "dad"
 
 #' SNP counts for the affected children of case-parent triads.
 #'
-#' A dataset containing the counts of the alternate allele
+#' A simulated dataset containing the counts of the alternate allele
 #' for 100 SNPs for the affected child in 1000 simulated case-parent
-#' triads. Columns represent snps, rows are individuals.
+#' triads. Columns represent snps, rows are individuals. SNPs
+#' in columns 51, 52, 76, and 77 represent a true risk pathway.
 #'
 #' @format A data frame with 1000 rows and 100 variables
 "case"
