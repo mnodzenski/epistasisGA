a39f9d5a6f1a55c2d37afb5868ad05dffb29478f
Michael.Nodzenski <michael.nodzenski@gmail.com>
2020-05-27 13:43:15 -0400

    use formatR packge to make sure lines are not too long.

--- R/network.threshold.R
+++ R/network.threshold.R
@@ -17,17 +17,17 @@
 #'                               matrix(rep(TRUE, 25^2), nrow = 25),
 #'                               matrix(rep(TRUE, 25^2), nrow = 25))))
 #'
-#  ## preprocess data
+# ## preprocess data
 #' pp.list <- preprocess.genetic.data(case[, 1:10], father.genetic.data = dad[ , 1:10],
 #'                                mother.genetic.data = mom[ , 1:10],
 #'                                chrom.mat = chrom.mat[ , 1:10])
 #' ## run GA for observed data
 #'
-#' run.ga(pp.list, n.chromosomes = 5, chromosome.size = 3, results.dir = "tmp_3",
-#'        cluster.type = "interactive", registryargs = list(file.dir = "tmp_reg", seed = 1500),
+#' run.ga(pp.list, n.chromosomes = 5, chromosome.size = 3, results.dir = 'tmp_3',
+#'        cluster.type = 'interactive', registryargs = list(file.dir = 'tmp_reg', seed = 1500),
 #'        generations = 2, n.islands = 2, island.cluster.size = 1, n.top.chroms = 3)
-#'  combined.res3 <- combine.islands("tmp_3")
-#'  unlink("tmp_reg", recursive = TRUE)
+#'  combined.res3 <- combine.islands('tmp_3')
+#'  unlink('tmp_reg', recursive = TRUE)
 #'
 #' ## create three permuted datasets
 #' set.seed(1400)
@@ -37,37 +37,37 @@
 #'                                   n.permutations = 3)
 #'
 #' ## pre-process permuted data
-#' p1.list <- preprocess.genetic.data(perm.data.list[["permutation1"]]$case,
-#'                                    complement.genetic.data = perm.data.list[["permutation1"]]$comp,
+#' p1.list <- preprocess.genetic.data(perm.data.list[['permutation1']]$case,
+#'                                    complement.genetic.data = perm.data.list[['permutation1']]$comp,
 #'                                    chrom.mat = chrom.mat[ , 1:10])
 #'
-#' p2.list <- preprocess.genetic.data(perm.data.list[["permutation2"]]$case,
-#'                                    complement.genetic.data = perm.data.list[["permutation2"]]$comp,
+#' p2.list <- preprocess.genetic.data(perm.data.list[['permutation2']]$case,
+#'                                    complement.genetic.data = perm.data.list[['permutation2']]$comp,
 #'                                    chrom.mat = chrom.mat[ , 1:10])
 #'
-#' p3.list <- preprocess.genetic.data(perm.data.list[["permutation3"]]$case,
-#'                                    complement.genetic.data = perm.data.list[["permutation3"]]$comp,
+#' p3.list <- preprocess.genetic.data(perm.data.list[['permutation3']]$case,
+#'                                    complement.genetic.data = perm.data.list[['permutation3']]$comp,
 #'                                    chrom.mat = chrom.mat[ , 1:10])
 #'
 #' ## run GA for permuted data
 #'
-#' run.ga(p1.list, n.chromosomes = 5, chromosome.size = 3, results.dir = "p1_tmp_3",
-#'        cluster.type = "interactive", registryargs = list(file.dir = "tmp_reg", seed = 1500),
+#' run.ga(p1.list, n.chromosomes = 5, chromosome.size = 3, results.dir = 'p1_tmp_3',
+#'        cluster.type = 'interactive', registryargs = list(file.dir = 'tmp_reg', seed = 1500),
 #'        generations = 2, n.islands = 2, island.cluster.size = 1, n.top.chroms = 3)
-#'  p1.combined.res3 <- combine.islands("p1_tmp_3")
-#'  unlink("tmp_reg", recursive = TRUE)
+#'  p1.combined.res3 <- combine.islands('p1_tmp_3')
+#'  unlink('tmp_reg', recursive = TRUE)
 #'
-#' run.ga(p2.list, n.chromosomes = 5, chromosome.size = 3, results.dir = "p2_tmp_3",
-#'        cluster.type = "interactive", registryargs = list(file.dir = "tmp_reg", seed = 1500),
+#' run.ga(p2.list, n.chromosomes = 5, chromosome.size = 3, results.dir = 'p2_tmp_3',
+#'        cluster.type = 'interactive', registryargs = list(file.dir = 'tmp_reg', seed = 1500),
 #'        generations = 2, n.islands = 2, island.cluster.size = 1, n.top.chroms = 3)
-#'  p2.combined.res3 <- combine.islands("p2_tmp_3")
-#'  unlink("tmp_reg", recursive = TRUE)
+#'  p2.combined.res3 <- combine.islands('p2_tmp_3')
+#'  unlink('tmp_reg', recursive = TRUE)
 #'
-#' run.ga(p3.list, n.chromosomes = 5, chromosome.size = 3, results.dir = "p3_tmp_3",
-#'        cluster.type = "interactive", registryargs = list(file.dir = "tmp_reg", seed = 1500),
+#' run.ga(p3.list, n.chromosomes = 5, chromosome.size = 3, results.dir = 'p3_tmp_3',
+#'        cluster.type = 'interactive', registryargs = list(file.dir = 'tmp_reg', seed = 1500),
 #'        generations = 2, n.islands = 2, island.cluster.size = 1, n.top.chroms = 3)
-#'  p3.combined.res3 <- combine.islands("p3_tmp_3")
-#'  unlink("tmp_reg", recursive = TRUE)
+#'  p3.combined.res3 <- combine.islands('p3_tmp_3')
+#'  unlink('tmp_reg', recursive = TRUE)
 #'
 #'  ## get threshold fitness score
 #'  network.threshold(combined.res3$unique.results,
@@ -76,50 +76,50 @@
 #'                         p3.combined.res3$unique.results),
 #'                    0.2)
 #'
-#'  lapply(c("tmp_3" ,"p1_tmp_3", "p2_tmp_3", "p3_tmp_3"), unlink, recursive = TRUE)
+#'  lapply(c('tmp_3' ,'p1_tmp_3', 'p2_tmp_3', 'p3_tmp_3'), unlink, recursive = TRUE)
 #'
 #' @importFrom  data.table rbindlist
 #' @export
 
-network.threshold <- function(observed.results, permutation.list, threshold.val){
-
-  #error checking
-  if (any(duplicated(observed.results$chromosome))){
-
-    stop("Observed results must included only unique chromosomes, duplicates detected.")
-
-  }
-
-  #observed fitness scores
-  obs.scores <- observed.results$fitness.score
-  obs.scores <- sort(obs.scores, decreasing = TRUE)
-  n.obs.scores <- length(obs.scores)
-
-  #permutation fitness scores
-  perm.scores <- unique(rbindlist(permutation.list)$fitness.score)
-  n.perm.scores <- length(perm.scores)
-
-  #find the observed fitness score closest to producing threshold.val
-  prop.higher <- rep(NA, n.obs.scores)
-  for (i in seq_len(n.obs.scores)){
-
-    val <- obs.scores[i]
-    prop.higher[i] <- sum(perm.scores > val)/n.perm.scores
-
-    if (prop.higher[i] >= threshold.val){
-
-      fitness.score.threshold <- val
-      prop.permutations.higher <- prop.higher[i - 1]
-      break
-
+network.threshold <- function(observed.results, permutation.list, threshold.val) {
+    
+    # error checking
+    if (any(duplicated(observed.results$chromosome))) {
+        
+        stop("Observed results must included only unique chromosomes, duplicates detected.")
+        
     }
-
-  }
-
-  #return the fitness score threshold and proportion of permutations with at least one
-  #fitness score higher than this value
-  return(list(fitness.score.threshold = fitness.score.threshold, prop.permutations.higher = prop.permutations.higher))
-
+    
+    # observed fitness scores
+    obs.scores <- observed.results$fitness.score
+    obs.scores <- sort(obs.scores, decreasing = TRUE)
+    n.obs.scores <- length(obs.scores)
+    
+    # permutation fitness scores
+    perm.scores <- unique(rbindlist(permutation.list)$fitness.score)
+    n.perm.scores <- length(perm.scores)
+    
+    # find the observed fitness score closest to producing threshold.val
+    prop.higher <- rep(NA, n.obs.scores)
+    for (i in seq_len(n.obs.scores)) {
+        
+        val <- obs.scores[i]
+        prop.higher[i] <- sum(perm.scores > val)/n.perm.scores
+        
+        if (prop.higher[i] >= threshold.val) {
+            
+            fitness.score.threshold <- val
+            prop.permutations.higher <- prop.higher[i - 1]
+            break
+            
+        }
+        
+    }
+    
+    # return the fitness score threshold and proportion of permutations with at least one fitness score
+    # higher than this value
+    return(list(fitness.score.threshold = fitness.score.threshold, prop.permutations.higher = prop.permutations.higher))
+    
 }
 
 
