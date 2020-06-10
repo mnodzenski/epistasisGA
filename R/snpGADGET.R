748180ca83337db3666311811ad55cb9b8a282d2
Michael.Nodzenski <michael.nodzenski.nih.gov>
2020-06-10 14:22:46 -0400

    Rename the package description file.

--- R/snpGADGET.R
+++ R/snpGADGET.R
@@ -1,15 +1,15 @@
-#' \code{knnGA} package
+#' \code{snpGADGET} package
 #'
 #' A package implementing a genetic algorithm to detect multi-SNP effects in case-parent triad studies.
 #'
 #' @docType package
-#' @name knnGA
+#' @name snpGADGET
 NULL
 
 ## quiets concerns of R CMD check re visible bindings of global vars created via data table
 if (getRversion() >= "2.15.1") {
-    
-    utils::globalVariables(c("chromosome", ".SD", "fitness.score", "raw.fitness.score", "min.elem", 
+
+    utils::globalVariables(c("chromosome", ".SD", "fitness.score", "raw.fitness.score", "min.elem",
         "Var1", "Var2", "name", "cluster", "job.id"))
-    
+
 }
