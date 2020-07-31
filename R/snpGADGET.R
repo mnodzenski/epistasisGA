33588a9354b6e828089399ddb54cfa9022f68603
mnodzenski <michael.nodzenski@gmail.com>
2020-07-31 16:15:05 -0400

    Account for more variables in data.table objects.

--- R/snpGADGET.R
+++ R/snpGADGET.R
@@ -10,6 +10,6 @@ NULL
 if (getRversion() >= "2.15.1") {
 
     utils::globalVariables(c("chromosome", ".SD", "fitness.score", "raw.fitness.score", "min.elem",
-        "Var1", "Var2", "name", "cluster", "job.id"))
+        "Var1", "Var2", "name", "cluster", "job.id", "V1", "V2", "edge.score", "h.score"))
 
 }
