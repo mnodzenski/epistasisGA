2a6aedbb810c7cb349c9b238d7e13290769d7011
Michael.Nodzenski <michael.nodzenski.nih.gov>
2020-08-18 15:22:18 -0400

    Use seq_len rather than colon.

--- R/snpGADGET.R
+++ R/snpGADGET.R
@@ -10,6 +10,7 @@ NULL
 if (getRversion() >= "2.15.1") {
 
     utils::globalVariables(c("chromosome", ".SD", "fitness.score", "raw.fitness.score", "min.elem",
-        "Var1", "Var2", "name", "cluster", "job.id", "V1", "V2", "edge.score", "h.score"))
+        "Var1", "Var2", "name", "cluster", "job.id", "V1", "V2", "edge.score", "h.score",
+        "..choose.these"))
 
 }
